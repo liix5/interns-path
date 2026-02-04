a7730d74d1df48c006702b95460d637759c2a6af
Layan  ALharthi <118828649+liix5@users.noreply.github.com>
2026-02-04 18:51:59 +0300

    no results message update

--- components/main/Experiences.tsx
+++ components/main/Experiences.tsx
@@ -1,5 +1,6 @@
 import ExperienceCardPreview from "@/components/main/ExperienceCardPreview";
 import { fetchFilteredExperiences } from "@/app/lib/data";
+import { Frown } from "lucide-react";
 
 type ExperiencesFeedProps = {
   page: number;
@@ -25,9 +26,17 @@ export default async function ExperiencesFeed({
       {/* Cards */}
       <ul className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 auto-rows-fr">
         {experiences?.length === 0 ? (
-          <p className="col-span-full my-3 text-center text-muted-foreground">
-            لا توجد نتائج تطابق بحثك
-          </p>
+          <>
+            <p className=" flex text-primary items-center justify-center mt-3  ">
+              لا توجد نتائج تطابق بحثك
+              <Frown className=" mr-2 text-primary justify-center" />
+            </p>
+            <p className="text-center mb-12 text-muted-foreground text-sm">
+              لم تجد تجارب في مجالك؟
+              <br />
+              شارك الموقع في مجتمعك وساهم في إضافة تجارب جديدة{" "}
+            </p>
+          </>
         ) : (
           experiences?.map((exp) => (
             <li key={exp.id}>
